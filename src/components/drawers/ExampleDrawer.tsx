1d8bed96edc00eaa6d8d45808e76639938435813
Connor Beleznay <67024888+Connorbelez@users.noreply.github.com>
2023-12-27 18:26:30 -0500

    drawer

--- src/components/drawers/ExampleDrawer.tsx
+++ src/components/drawers/ExampleDrawer.tsx
@@ -122,21 +122,7 @@ export default function DrawerDemo() {
                 <span className="sr-only">Increase</span>
               </Button>
             </div>
-            <div className="mt-3 h-[120px]">
-              {/* <ResponsiveContainer width="100%" height="100%">
-                <BarChart data={data}>
-                  <Bar
-                    dataKey="goal"
-                    style={
-                      {
-                        fill: "hsl(var(--foreground))",
-                        opacity: 0.9,
-                      } as React.CSSProperties
-                    }
-                  />
-                </BarChart>
-              </ResponsiveContainer> */}
-            </div>
+  
           </div>
           <DrawerFooter>
             <Button>Submit</Button>
