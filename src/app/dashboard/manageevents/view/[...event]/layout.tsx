a2dd26bbd391d3d83e44490c3911a69f6e40ce8d
Connor Beleznay <67024888+Connorbelez@users.noreply.github.com>
2024-01-14 23:16:32 -0500

    build

--- src/app/dashboard/manageevents/view/[...event]/layout.tsx
+++ src/app/dashboard/manageevents/view/[...event]/layout.tsx
@@ -3,10 +3,12 @@ import React from "react";
 // import { useLockedBody } from "./components/hooks/useBodyLock";
 // import { SidebarWrapper } from "./components/sidebar/sidebar";
 // import { SidebarContext } from "./components/ly/layout-context"
-import NavPopup from "./components/NavPopup/NavPopup";
-import SideNav from "./components/SideNav/SideNav";
+// import NavPopup from "./components/NavPopup/NavPopup";
+// import SideNav from "./components/SideNav/SideNav";
+import NavPopup from "@/app/dashboard/components/NavPopup/NavPopup";
+import SideNav from "@/app/dashboard/components/SideNav/SideNav";
 import dynamic from "next/dynamic";
-const SideBarWrapper = dynamic(() => import("./components/sidebar/sidebar"))
+const SideBarWrapper = dynamic(() => import("@/app/dashboard/components/sidebar/sidebar"))
 interface Props {
   children: React.ReactNode;
 }
